89bd5835cae1e1d4bc52963ff637ea7dd747df34
Jack Shao <jacky.shaoxg@gmail.com>
2014-11-03 17:30:17 +0800

    update to run on seeeduino cloud
    - run on seeeduino cloud's Leonardo
    - sensors: air quality, sound, uv, light, temp & humility, dust

--- Aetheraduino/AetheraSerial/AetheraSerial.ino
+++ Aetheraduino/AetheraSerial/AetheraSerial.ino
@@ -1,179 +1,487 @@
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BMP085_U.h>
-#include <Adafruit_SI1145.h>
+/***********************************************************
+  * AetheraSerial.ino
+  *
+  * Description:
+  *   The Arduino part of the Aethera project.
+  *   Read sensor values, output them through the serial
+  * Create Date: 2014/10
+  * Change Log:  Inital version - seeedstudio
+  *
+***********************************************************/
+/* Including files */
 #include <DHT.h>
-#define DHTPIN 5  // Temp/Humidity connected to Digital Pin 5
-#define DHTTYPE DHT11   // DHT 22  (AM2302)
+#include <Wire.h>
+#include <Digital_Light_TSL2561.h>
 
-Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
-Adafruit_SI1145 uv = Adafruit_SI1145();
+/* Sensor pin */
+#define pin_uv              A2      // UV  sensor
+#define pin_sound           A0      // Sound sensor
+#define pin_dust            7       // Dust sensor
+#define pin_air_quality     A1      // Air quality sensor
+#define pin_dht             4       // Humidity and temperature sensor
+#define DHTTYPE             DHT22   // DHT 22  (AM2302)
 
-DHT dht(DHTPIN, DHTTYPE);
+//digital light sensor      I2C
 
-//Electret mic is connected to A0
-int electretPin = 0;  
-int electretReading;
+#define SER_PUSH      Serial        //change to Serial1 for pushing to openWrt side through bridge
 
-//Sharp Dust sensor is connected to A1
-int particulatePin = 1;  
-int particulateReading;     
-int ledPowerPin=6;       //Wire 3 from the Sharp sensor is connected to D6
-//Manage the particulate sensor
-int delayTime=280;
-int delayTime2=40;
-float offTime=50680;
-//Calculate particulate information
-int i=0;
-float ppm=0;
-char s[32];
-float voltage = 0;
-float dustdensity = 0;
-float ppmpercf = 0;
-float dustdensityMax=0;
-float ppmMax = 0;  
+/* sample function */
+float iReadTemperature(void);
+float iReadHumidity(void);
+float iReadDensityDust(void);
+unsigned long iReadLux(void);
+float iReadUVRawVol(void);
+int iReadSoundRawVol(void);
+
+/* dust variables */
+unsigned long dust_starttime;
+unsigned long duration_starttime;
+unsigned long duration;
+unsigned long sampletime_ms = 30000;
+unsigned long lowpulseoccupancy = 0;
+float ratio = 0;
+float concentration = -1;
+int sensorValue=0;
+
+/* temperature and humidity sensor */
+DHT dht(pin_dht, DHTTYPE);
+
+/* sound sensor */
+float sound_raw_vol_max = 0.0f;  //mV
+
+/* Air Quality */
+enum
+{
+  AQ_INIT, AQ_WAIT_INIT, AQ_WORK
+};
+enum
+{
+  AQ_HIGH_POLLUTION, AQ_POLLUTION, AQ_LOW_POLLUTION, AQ_FRESH, AQ_WARMUP
+};
+unsigned long air_quality_sensor_init_starttime;
+int air_quality_sensor_state = AQ_WAIT_INIT;
+unsigned long air_quality_sensor_init_time = 200000;
+int aq_first_vol, aq_last_vol, aq_std_vol, aq_std_vol_sum;
+static int cntr_aq_avg = 0;
+int aq_result = AQ_WARMUP;
 
 
-int airqualityPin = 2;  ////MQ-135  is connected to A2
-float airqualityReading;  
+/* Global varibles */
+boolean valid_dust = false;
+unsigned long push_starttime;
+unsigned long push_interval = 1000;  //ms
 
+
+
+
+//*****************************************************************************
+//
+//! \brief setup
+//!
+//! \param[in]
+//! \param[in]
+//!
+//! \return
+//!
+//*****************************************************************************
 void setup() {
-  Serial.begin(57600); 
-   // Serial.println("Aethera!");
-   
-   //Driver for Sharp Dust Sensor LED
-   pinMode(ledPowerPin,OUTPUT);
-  
-    //Begin I2C Sensor Readings, both sensors are connected to SDA and SCL pins
-   if(! bmp.begin())  
-  {
-    Serial.print("Ooops, no BMP085 detected ... Check your wiring or I2C ADDR!");
-    while(1);
-  }
-   if (! uv.begin()) {
-    Serial.print("Ooops, no Si1145 detected ... Check your wiring or I2C ADDR!");
-    while (1);
-  }
-  
+
+  // for debugging, wait until a serial console is connected
+  Serial.begin(115200);    //Serial for debugging
+  Serial1.begin(115200);   //Serial1 for outputing data to OpenWrt, serial-bridge
+  delay(2000);  //wait usb serial to be ready
+
+  /* Wire Begin */
+  Wire.begin();
+
+  Serial.println(F("******Aethera Sensor Node******\r\n"));
+
+  /* Initialize temperature and humidity sensor */
+  Serial.println(F("Initialize temper humidity sensor...\r\n"));
   dht.begin();
-  //initialize timer and particulate reading
-  i=0;
-  ppm=0;
+
+  /* Initialize Dust sensor */
+  digitalWrite(pin_dust, HIGH);
+  pinMode(pin_dust,INPUT);
+  attachInterrupt(0, dust_interrupt, CHANGE);
+
+  /* Digital Light Sensor */
+  TSL2561.init();
+
+  /* Sound Sensor */
+  pinMode(pin_sound, INPUT);
+
+  /* UV Sensor */
+  pinMode(pin_uv, INPUT);
+
+  /* Air Quality */
+  pinMode(pin_air_quality, INPUT);
+  air_quality_sensor_init_starttime = millis();
+
+
+  /* other */
+  push_starttime = millis();
+  init_timer1(200);  //us, get the value based on Shannon's law, sound freq: 0~3400Hz
 }
 
-void loop() {
 
-  electretReading = analogRead(electretPin);
-  airqualityReading = analogRead(airqualityPin); 
- 
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float humidity = dht.readHumidity();
-  // Read temperature as Celsius
-  float tempC = dht.readTemperature();
-  // Read temperature as Fahrenheit
-  float tempF = dht.readTemperature(true);
-  
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(humidity) || isnan(tempC) || isnan(tempF)) {
-    Serial.print("000"); //000 indicates a failure in the data and on the graph. 
-    Serial.print(",");
-    return;
-  }
+//*****************************************************************************
+//
+//! \brief loop
+//!
+//! \param[in]
+//! \param[in]
+//!
+//! \return
+//!
+//*****************************************************************************
+void loop()
+{
+  //real time task
+  air_quality_state_machine();
 
-  // Compute heat index
-  // Must send in temp in Fahrenheit!
-  float heatIndex = dht.computeHeatIndex(tempF, humidity);
-
-  // the index is multiplied by 100 so to get the integer index, divide by 100!
-  float uvIndex = uv.readUV();
-  uvIndex /= 100.0;  
-  
- //Serial.print("/Visible Light/"); 
-  Serial.print(uv.readVisible());
-  Serial.print(",");  
- //Serial.print("/Sound/");
-  Serial.print(electretReading);
-  Serial.print(","); 
- //Serial.print("/AirQuality/");
-  Serial.print(airqualityReading);
-  Serial.print(","); 
- //Serial.print("/Temperature/"); 
-  Serial.print(tempF);
-  Serial.print(","); 
-  //Serial.print("/Humidity/"); 
-  Serial.print(humidity);
-  Serial.print(","); 
- //Serial.print("/HeatIndex/");
-  Serial.print(heatIndex);
-  Serial.print(","); 
- // Serial.print("/UV Index/: ");  Serial.print(","); 
-  Serial.print(uvIndex);
-  Serial.print(",");  
-  
-  sensors_event_t event;
-  bmp.getEvent(&event);
- 
-  /* Display the results (barometric pressure is measure in hPa) */
-  if (event.pressure)
-  {
-    /* Display atmospheric pressue in hPa */
-    //Serial.print("/Pressure/");
-    Serial.print(event.pressure);
-    Serial.print(",");  
-    //  Serial.println(" hPa");
-    /* Get the current temperature from the BMP085, only for calculating Pressure */
-    float temperature;
-    bmp.getTemperature(&temperature);
-  }
-  else
-  {
-    Serial.println("Sensor error");
-  }
-  
- 
-  //Sharp Code
-  i=i+1;
-  digitalWrite(ledPowerPin,LOW); // power on the LED
-  delayMicroseconds(delayTime);
-    particulateReading=analogRead(particulatePin); // read the dust value
-    ppm = ppm+particulateReading;
-    // Serial.println(particulateReading);
-  delayMicroseconds(delayTime2);
-  digitalWrite(ledPowerPin,HIGH); // turn the LED off
-  delayMicroseconds(offTime);
-  
-  voltage = ppm/i*0.0049; // 5V / 1024 = 0.0049
-  dustdensity = 0.17*voltage-0.1;
-  ppmpercf = (voltage-0.0256)*120000;
-    if (ppmpercf < 0)
-      ppmpercf = 0;
-    if (ppmpercf > ppmMax) 
-      ppmMax = ppmpercf;   //calculate max value over sampled time
-    if (dustdensity < 0 )
-      dustdensity = 0;
-    if (dustdensity > 0.5)
-      dustdensity = 0.5;
-    if (dustdensity > dustdensityMax) 
-      dustdensityMax = dustdensity;
- 
-    String particulateDensityString;
-    String ppmString;
-    particulateDensityString = dtostrf(dustdensity, 5, 2, s);
-    ppmString = dtostrf(ppmpercf, 8, 0, s);
-    
-    i=0;
-    ppm=0;   
-    int dustdensityMax=0;
-    int ppmMax=0;  
-    
-    //Serial.print("/PPM/");
-    Serial.print(ppmString);
-    Serial.print(",");
-    // Serial.print("/Particulate Density/");
-    Serial.println(particulateDensityString);
-    // Serial.println();
-    
-    // Wait a few seconds between measurements.
-    delay(offTime);
+  //slow task
+  push_data();
 }
+
+//*****************************************************************************
+//
+//! \brief push data to OpenWrt side
+//!
+//!
+//! \return void
+//!
+//*****************************************************************************
+void push_data()
+{
+  if(millis() - push_starttime > push_interval)
+  {
+    push_starttime = millis();
+
+    SER_PUSH.print(F("/Visible Light/"));
+    SER_PUSH.print(iReadLux());  //>100ms , lux
+    SER_PUSH.print(",");
+
+    SER_PUSH.print(F("/Temperature/"));
+    SER_PUSH.print(iReadTemperature());  //>250ms, F
+    SER_PUSH.print(",");
+
+    SER_PUSH.print(F("/Humidity/"));
+    SER_PUSH.print(iReadHumidity());  //>250ms, %
+    SER_PUSH.print(",");
+
+    SER_PUSH.print(F("/Particulate Density/"));
+    SER_PUSH.print(iReadDensityDust());  // ~ 0ms, pcs/0.01cf or pcs/283ml
+    SER_PUSH.print(",");
+
+    SER_PUSH.print(F("/UV Raw/"));
+    SER_PUSH.print(iReadUVRawVol());  // > 128ms, mV
+    SER_PUSH.print(",");
+
+    SER_PUSH.print(F("/Sound Raw/"));
+    SER_PUSH.print(iReadSoundRawVol());  // ~0ms, mV
+    SER_PUSH.print(",");
+
+    SER_PUSH.print(F("/Air Quality/"));  //~0ms
+    switch (aq_result)
+    {
+      case AQ_WARMUP:
+        SER_PUSH.print(F("WarmUp")); break;
+      case AQ_FRESH:
+        SER_PUSH.print(F("Fresh")); break;
+      case AQ_LOW_POLLUTION:
+        SER_PUSH.print(F("LowPollution")); break;
+      case AQ_POLLUTION:
+        SER_PUSH.print(F("Pollution")); break;
+      case AQ_HIGH_POLLUTION:
+        SER_PUSH.print(F("HighPollution")); break;
+    }
+
+    SER_PUSH.println();  //sum ~ 800ms / push
+  }
+}
+
+/* Interrupt service */
+void dust_interrupt()
+{
+  if (digitalRead(pin_dust) == 0)  //fall
+  {
+    duration_starttime = millis();
+  }else
+  {
+    duration = millis() - duration_starttime;
+    if (duration > 10 && duration < 200)
+    {
+      lowpulseoccupancy+=duration;
+    }
+    if ((millis()-dust_starttime) > sampletime_ms)
+    {
+      valid_dust = true;
+      ratio = lowpulseoccupancy/(sampletime_ms*10.0);  // Integer percentage 0=>100
+      concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // using spec sheet curve
+      lowpulseoccupancy = 0;
+      dust_starttime = millis();
+    }
+  }
+
+}
+
+/* Timer1 Service */
+static int cntr_aq_sample = 0;
+ISR(TIMER1_OVF_vect)
+{
+  int snd_raw = analogRead(pin_sound);
+  float snd_raw_mv = snd_raw * (4980.0 / 1023.0);
+  if (snd_raw_mv > sound_raw_vol_max) sound_raw_vol_max = snd_raw_mv;
+
+  if (air_quality_sensor_state == AQ_WORK && ++cntr_aq_sample == 10000)  //200us * 10000 = 2s
+  {
+    cntr_aq_sample = 0;
+    //Serial.println("tm1 isr");
+    air_quality_sensor_evaluation();
+  }
+}
+
+
+//*****************************************************************************
+//
+//! \brief push data to OpenWrt side
+//!
+//!
+//! \return void
+//!
+//*****************************************************************************
+void air_quality_state_machine()
+{
+  switch (air_quality_sensor_state)
+  {
+    case AQ_WAIT_INIT:
+      {
+        if (millis() - air_quality_sensor_init_starttime > air_quality_sensor_init_time)
+        {
+          air_quality_sensor_state = AQ_INIT;
+        }
+        break;
+      }
+    case AQ_INIT:
+      {
+        int v = analogRead(pin_air_quality);
+        if (v < 798 && v > 10)  //the init voltage is ok
+        {
+          //Serial.print("init:");
+          //Serial.println(v);
+
+          aq_first_vol = v;
+          aq_last_vol = v;
+          aq_std_vol = v;
+          aq_std_vol_sum = 0;
+          air_quality_sensor_state = AQ_WORK;
+        }
+        else
+        {
+          air_quality_sensor_init_starttime = millis();
+          air_quality_sensor_state = AQ_WAIT_INIT;
+        }
+        break;
+      }
+    case AQ_WORK:
+      {
+        break;
+      }
+    default:
+      break;
+  }
+}
+
+//*****************************************************************************
+//
+//! \brief do a window average
+//!
+//!
+//! \return void
+//!
+//*****************************************************************************
+void air_quality_sensor_window_avg()
+{
+  if (++cntr_aq_avg >= 150)  //sum for 5 minutes
+  {
+    cntr_aq_avg = 0;
+    aq_std_vol = aq_std_vol_sum / 150;
+    aq_std_vol_sum = 0;
+  } else
+  {
+    aq_std_vol_sum += aq_first_vol;
+  }
+}
+
+//*****************************************************************************
+//
+//! \brief do a window average
+//!
+//!
+//! \return void
+//!
+//*****************************************************************************
+void air_quality_sensor_evaluation()
+{
+  aq_last_vol = aq_first_vol;
+  aq_first_vol = analogRead(pin_air_quality);
+
+  //Serial.println(aq_std_vol);
+  //Serial.println(aq_first_vol);
+
+  if (aq_first_vol - aq_last_vol > 400 || aq_first_vol > 700)
+  {
+    aq_result = AQ_HIGH_POLLUTION;
+  } else if ((aq_first_vol - aq_last_vol > 400 && aq_first_vol < 700) || aq_first_vol - aq_std_vol > 150)
+  {
+    aq_result = AQ_POLLUTION;
+  } else if ((aq_first_vol - aq_last_vol > 200 && aq_first_vol < 700) || aq_first_vol - aq_std_vol > 50)
+  {
+    aq_result = AQ_LOW_POLLUTION;
+  } else
+  {
+    aq_result = AQ_FRESH;
+  }
+  air_quality_sensor_window_avg();
+}
+
+//*****************************************************************************
+//
+//! \brief setup the time 1 of 32u4
+//!
+//! \param[in]
+//! \param[in]
+//!
+//! \return  voltage mV
+//!
+//*****************************************************************************
+#define RESOLUTION 65536    // Timer1 is 16 bit
+void init_timer1(long us)
+{
+  TCCR1A = 0;                 // clear control register A
+  TCCR1B = _BV(WGM13);        // set mode as phase and frequency correct pwm, stop the timer
+
+  long cycles;
+  long microseconds = us;   //setup microseconds here
+  unsigned char clockSelectBits;
+  cycles = (F_CPU / 2000000) * microseconds;                                // the counter runs backwards after TOP, interrupt is at BOTTOM so divide microseconds by 2
+  if (cycles < RESOLUTION)              clockSelectBits = _BV(CS10);              // no prescale, full xtal
+  else if ((cycles >>= 3) < RESOLUTION) clockSelectBits = _BV(CS11);              // prescale by /8
+  else if ((cycles >>= 3) < RESOLUTION) clockSelectBits = _BV(CS11) | _BV(CS10);  // prescale by /64
+  else if ((cycles >>= 2) < RESOLUTION) clockSelectBits = _BV(CS12);              // prescale by /256
+  else if ((cycles >>= 2) < RESOLUTION) clockSelectBits = _BV(CS12) | _BV(CS10);  // prescale by /1024
+  else        cycles = RESOLUTION - 1, clockSelectBits = _BV(CS12) | _BV(CS10);  // request was out of bounds, set as maximum
+
+  ICR1 = cycles;
+  TCCR1B &= ~(_BV(CS10) | _BV(CS11) | _BV(CS12));
+  TCCR1B |= clockSelectBits;                                          // reset clock select register, and starts the clock
+
+  TIMSK1 = _BV(TOIE1);
+  TCNT1 = 0;
+  sei();                      //enable global interrupt
+}
+
+//*****************************************************************************
+//
+//! \brief Read temperature
+//!
+//! cost time: > 250ms
+//!
+//! \param[in]
+//! \param[in]
+//!
+//! \return  temperature
+//! \refer to http://www.seeedstudio.com/wiki/File:Humidity_Temperature_Sensor_pro.zip
+//*****************************************************************************
+float iReadTemperature(void) {
+    float temper;
+    temper = dht.readTemperature(false); //true: get F, false: get oC
+    return temper;
+}
+//*****************************************************************************
+//
+//! \brief Read Humidity
+//!
+//! cost time: > 250ms
+//!
+//! \param[in]
+//! \param[in]
+//!
+//! \return  Humidity
+//!
+//*****************************************************************************
+float iReadHumidity(void) {
+    float humidity;
+    humidity = dht.readHumidity();
+    return humidity;
+}
+
+//*****************************************************************************
+//
+//! \brief Read Dust Density
+//!
+//! cost time: ~0ms , unit: pcs/0.01cf or pcs/283ml
+//!
+//! \param[in]
+//! \param[in]
+//!
+//! \return  temperature
+//!
+//*****************************************************************************
+float iReadDensityDust(void) {
+    return concentration;
+}
+
+//*****************************************************************************
+//
+//! \brief Read Lux value of visible light
+//!
+//! \param[in]
+//! \param[in]
+//!
+//! \return  Luminance
+//!
+//*****************************************************************************
+unsigned long iReadLux(void) {
+    //cost time: > 100ms
+    return TSL2561.readVisibleLux();
+}
+//*****************************************************************************
+//
+//! \brief Read the raw voltage signal of UV sensor
+//!
+//! \param[in]
+//! \param[in]
+//!
+//! \return  voltage mV
+//!
+//*****************************************************************************
+float iReadUVRawVol(void) {
+    unsigned long sum=0;
+    for(int i=0; i<128; i++)
+    {
+      sum += analogRead(pin_uv);
+      delay(1);
+    }
+    sum >>= 7;
+    return sum*(4980.0f/1023.0f);
+}
+
+//*****************************************************************************
+//
+//! \brief Read the raw voltage signal of sound sensor
+//!
+//! \param[in]
+//! \param[in]
+//!
+//! \return  voltage mV
+//!
+//*****************************************************************************
+int iReadSoundRawVol()
+{
+  int tmp = sound_raw_vol_max;
+  sound_raw_vol_max = 0;
+  return tmp;
+}
+
